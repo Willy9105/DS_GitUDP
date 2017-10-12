dc8bf7885ca2c4effeb15a3d4c2ea78edc4c5fde
Willy9105 <larreura@lacroixrouge-brest.fr>
2017-10-12 15:05:05 +0200

    commit

--- client_UDP.py
+++ client_UDP.py
@@ -2,20 +2,25 @@
 
 import socket
 
+ip='192.168.0.202'      #adresse ip du serveur
+port= 5005         #port du serveur
+
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.settimeout(1.0)
 
-sock.connect(???)
-sock.send(???)
+sock.sendto("cinema",(ip,port))     #Envoi de la chaine de caracteres "cinema" vers le serveur
 
 
-trameReponse, addr = sock.recvfrom(1024)
+trameReponse,addr=sock.recvfrom(1024)           #Permet de recevoir un message qui provient du serveur avec un buffer de taille 1024
 
-print "Réception de la trame de réponse", trameReponse.encode("hex")
+print "Réception de la trame de réponse", trameReponse.encode("hex")       #On affiche le message que l'on a reçu en hexa
 
-code = ???
+b0=trameReponse.encode("hex") #On met la trame dans une variable
 
-print code
+code=int(b0, 32)    
+print "Le code est :", str(code)
+
+
 
